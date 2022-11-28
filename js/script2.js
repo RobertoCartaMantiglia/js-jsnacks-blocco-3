d771a7ca3b07b778c7366f2701fefe2a626dcbd6
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-28 16:43:03 +0100

    second exercise, donesn't work

---
+++ js/script2.js
@@ -0,0 +1,13 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+//Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+
+const listNumbers = [];
+
+let userNumbers = parseInt(prompt("Inserisci un numero"));
+
+while(listNumbers[i < 50]) {
+    userNumbers = parseInt(prompt("Inserisci un numero"));
+    i++;
+}
+console.log(listNumbers);
