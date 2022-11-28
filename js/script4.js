ee22150367ed11e15263518a27b2a69faf5e6bd2
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-28 18:08:00 +0100

    ex 4

---
+++ js/script4.js
@@ -0,0 +1,9 @@
+// Prendere due array della stessa lunghezza e generarne un terzo,
+// prendendo alternativamente gli elementi da uno e dall'altro es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+let arrayA = ["a", "b", "c"];
+let arrayB = [1, 2, 3];
+
+let arrayC = arrayA.concat(arrayB);
+console.log(arrayC);
