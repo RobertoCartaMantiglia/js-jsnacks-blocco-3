b34ce2c88c26c17aee37a62f3813e5ecc40161c6
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-29 12:04:28 +0100

    ex 5 get random number

---
+++ js/script5.js
@@ -0,0 +1,12 @@
+/**
+ * Crea una funzione che accetti due argomenti, e che restituisca un valore numerico randomico
+ *  tra i due argomenti inclusi.
+ */
+
+function getRandomNumber (numMin, numMax){
+    const randomNumber = Math.floor(Math.random()*(numMax - numMin) + numMin);
+    return randomNumber;
+}
+
+let numeroCasuale = getRandomNumber(0, 10);
+console.log(numeroCasuale);
