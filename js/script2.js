3321b5204680580df011d70b43178969932e06f9
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-29 11:23:59 +0100

    remake ex 2

--- js/script2.js
+++ js/script2.js
@@ -1,17 +1,15 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 //Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-
-const listNumbers = [];
-let userNumbers;
-
+let numbers = [];
 let sum = 0;
 
 
-while(sum < 50) {
-    userNumbers = parseInt(prompt("Inserisci un numero"));
+
+while (sum < 50) {
+    const userNumbers = parseInt(prompt('inserisci un numero'));
     sum += userNumbers;
-    listNumbers.push(userNumbers);
+    numbers.push(userNumbers);
+    console.log(numbers);
 }
-console.log(listNumbers);
 console.log(sum);
