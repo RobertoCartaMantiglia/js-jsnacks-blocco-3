639ecdf98f67ed229cf2d0c3d8dcc824e2693482
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-02 16:48:05 +0100

    remake ex 5

--- js/script6.js
+++ js/script6.js
@@ -4,21 +4,13 @@
  *
  */
 
-let lista = [2, 8, 12, 33];
+let array = [1, 5, 18, 75];
 
-
-
-function getSumNumbers (array){
+function sumArray(array) {
     let sum = 0;
-
-    for (let i=0; i < array.length; i++){
-
-        const castedElement = parseInt(lista[i], 10);   //castedelement è una variabile che prende i valori interi del array su BASE 10 (... , 10)
-       
-        if(! Number.isNaN(castedElement)){
-            sum = sum +castedElement;
-        }
-    }   
-    return sum;
+    for (let i = 0; i < array.length; i++) {
+        sum += (array[i]);
+    }
+    console.log(sum);
 }
-console.log(getSumNumbers(lista));
+sumArray(array);
